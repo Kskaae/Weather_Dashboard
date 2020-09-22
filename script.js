268df8b7239c6bc615654bdde83f836f3a0c7756
Kristopher Kaae <kskaae@gmail.com>
2020-09-21 22:36:17 -0700

    funtional ajax call added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    let apiKey = "d34d2fad2189d0cb0405954862d69d92"
-    let queryURL = "api.openweathermap.org/data/2.5/weather?q={Boise}&appid={d34d2fad2189d0cb0405954862d69d92}"
+    const apiKey = "d34d2fad2189d0cb0405954862d69d92"
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Boise&appid=" + apiKey;
     console.log(queryURL)
-    append
+
     let location = "";
     // AS A traveler
     // I WANT to see the weather outlook for multiple cities
@@ -18,24 +18,28 @@ $(document).ready(function () {
 
 
     // WHEN I search for a city
-    function searchBtn() {
-        let searchbtn = "searchWeather";
-        let(str).searchbtn("");
-        document.getElementById("button").innerHTML = null;
-    }
+    $(".btn-submit").click(function () {
+        console.log("here")
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+const data = response
+$("#boise .temp").text("temperature " + data.main.temp )
+        })
+    })
+
     // THEN I am presented with current and future conditions for that city and that city is added to the search history
     //Ajax call
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response)
-    })
+
     // WHEN I view current weather conditions for that city
-    $("#city").html("<card>" + response.name + "<card>");
-    $("#temp").text("temperature:" + temp);
-    $("#humidity").text("Humidity: " + response.main.humidity + "%");
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+    //     $("#city").html("<card>" + response.name + "<card>");
+    //     $("#temp").text("temperature:" + temp);
+    //     $("#humidity").text("Humidity: " + response.main.humidity + "%");
+    //     $("#wind") 
+});
+// THEN I am presented with the city name, the icon re-date, ane presentation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 // WHEN I view the UV index
 // Create UV index ajax call 
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
